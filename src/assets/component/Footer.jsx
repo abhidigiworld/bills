93fbd11f6ef6515c0d91c5acaaf4fd9ca622fc66
abhidigiworld <abhidigiworld@gmail.com>
2024-05-27 14:18:10 +0530

    footer css chnage

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -3,7 +3,7 @@ import './printStyles.css';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 print-hidden">
+    <footer className="fixed bottom-0 w-full bg-gray-800 text-white py-4 print-hidden">
       <div className="container mx-auto text-center">
         <p>&copy; Sakshi Enterprises {new Date().getFullYear()}</p>
       </div>
