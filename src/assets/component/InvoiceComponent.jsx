af6ea8ff5f86b5cb7a3c204ad65b0da3f5123b72
abhidigiworld <abhidigiworld@gmail.com>
2024-05-28 16:36:49 +0530

    -

--- src/assets/component/InvoiceComponent.jsx
+++ src/assets/component/InvoiceComponent.jsx
@@ -115,7 +115,7 @@ function InvoiceComponent({ invoiceDetails }) {
       
         // Convert decimal part to words
         if (decimalPart > 0) {
-          words += 'and' + convertToWords(decimalPart) + 'Paisa ';
+          words += ' and ' + convertToWords(decimalPart) + 'Paisa ';
         }
       
         return words.trim();
