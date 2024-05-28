5bb9f0dfbeb2ece3317ef53f31698e66fcac1b32
abhidigiworld <abhidigiworld@gmail.com>
2024-05-28 17:31:44 +0530

    styling css

--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -7,12 +7,12 @@ function Header() {
   return (
     <header className="bg-blue-500 text-white py-4 print-hidden">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-2xl font-bold flex items-center">
           <Link to={'/Main'} className="flex items-center">
             <img src={logo} alt="Sakshi Enterprises Logo" className="h-8 mr-2" /> Sakshi Enterprises
           </Link>
         </h1>
-        <p>Providing Quality Services Since 2024</p>
+        <p className="text-right hidden md:block">Providing Quality Services Since 2024</p>
       </div>
     </header>
   );
