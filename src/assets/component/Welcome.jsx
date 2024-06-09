1876e1d64284e481a4ebf5cd70e65570b4d93343
abhidigiworld <abhidigiworld@gmail.com>
2024-06-09 13:19:24 +0530

    -

--- src/assets/component/Welcome.jsx
+++ src/assets/component/Welcome.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import bouncingImage from '../images/chair.png'; 
 import './printStyles.css';
 
 function WelcomePage() {
