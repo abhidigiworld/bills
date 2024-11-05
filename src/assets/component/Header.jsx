eff83fba19ab377553b82ed3d60a36b40fb6e34a
abhidigiworld <abhidigiworld@gmail.com>
2024-11-05 08:30:36 +0530

    Log out button added

--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './printStyles.css';
 import logo from '../images/LOGO.png'; 
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/'); 
+  };
+
   return (
     <header className="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 print-hidden">
       <div className="container mx-auto flex justify-between items-center">
@@ -13,6 +19,11 @@ function Header() {
           </Link>
         </h1>
         <p className="text-right hidden md:block">Providing Quality Services Since 2024</p>
+        <button 
+          onClick={handleLogout} 
+          className="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 ml-4">
+          Logout
+        </button>
       </div>
     </header>
   );
